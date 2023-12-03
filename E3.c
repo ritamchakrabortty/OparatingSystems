ebc4800c188b539985be91e631c9fa884f7299c5
Ritam Chakrabortty <107584853+ritamchakrabortty@users.noreply.github.com>
2023-12-03 21:28:32 +0530

    Add files via upload

---
+++ E3.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<unistd.h>
+#include <fcntl.h>
+#include<sys/wait.h>
+int main()
+{
+  int arr[] = {1,2,3,4,1,2};
+  int arrSize = sizeof(arr)/sizeof(int);
+  int start,end;
+  int fd[2];
+  if(pipe(fd) == -1)
+  {
+    return 1;
+  }
+  int id = fork();
+  if(id == -1)
+  {
+    return 2;
+  }
+  if(id == 0)
+  {
+    start = 0;
+    end = arrSize/2;
+  }
+  else
+  {
+    start = arrSize/2;
+    end = arrSize;
+  }
+  int sum =0;
+  int i;
+  for(i = start;i<end;i++)
+  {
+    sum += arr[i];
+  }
+  printf("Calculated partial sum: %d\n",sum);
+  if(id == 0)
+  {
+    close(fd[0]);
+    write(fd[1], &sum, sizeof(sum));
+    close(fd[1]);
+  }
+  else
+  {
+    int sumFromChild;
+    close(fd[1]);
+    read(fd[0],&sumFromChild,sizeof(sumFromChild));
+    close(fd[0]);
+    
+    int totalSum = sum + sumFromChild;
+    printf("Total  sum is %d\n",totalSum);
+    wait(NULL);
+  }
+  
+  
+  return 0;
+}
